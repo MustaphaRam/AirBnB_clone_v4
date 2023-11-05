92d55dd651496940aff1226558669077ab49904b
MustaphaRam <125461674+MustaphaRam@users.noreply.github.com>
2023-11-06 00:29:54 +0100

    Update 1-hbnb.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,19 @@
 $(document).ready(() => {
   let amenities = {};
 	
-  $('input[type="checkbox"]').change(() => {
+  $('input[type="checkbox"]').change( function () {
     if (this.checked) {
-      amenities[$(this).data('id')] = $(this).data('name');
-    } else {
-      delete amenities[$(this).data('id')];
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if (!$(this).prop('checked')) {
+      delete amenities[$(this).attr('data-id')];
     }
-    $('.amenities h4').text(Object.values(amenities).join(', '));
+    if (Object.keys(amenities).length === 0) {
+      $('div.amenities h4').html('&nbsp');
+    } else {
+      $('div.amenities h4').text(Object.values(amenities).join(', '));
+      if ( $('div.amenities h4').text().length > 30 ) {
+        $('div.amenities h4').text($('div.amenities h4').text().slice(0, 30) + '...');
+      }
+    }
   });
 });
